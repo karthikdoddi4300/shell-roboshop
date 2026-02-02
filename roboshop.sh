129c0edad53d5140b336453d8a5b470c8e1fe8bd
karthikdoddi4300 <karthikdoddi4300@gmail.com>
2026-02-02 18:43:43 +0530

    shell-practice

---
+++ roboshop.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+AMI_ID="ami-0220d79f3f480ecf5"
+SGID="sg-0d9028f369846db6c"
+
+for instance in $@
+do 
+    INSTANCE_ID=$(aws ec2 run-instances \
+        --image-id $AMI_ID\
+        --instance-type t3.micro \
+        --security-group-ids $SGID \
+        --key-name karthikkeypai \
+        --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=catalogue}]'\
+        --query 'Instances[0].InstanceId' \
+        --output text 
+        )
+    if [ $instance == frontend ]; then
+         IP=$(
+            aws ec2 describe-instances\
+            --instance-ids $INSTANCE_ID\
+            --query 'Reservations[].Instances[].PublicIpAddress' \
+            --output text
+          )
+    else 
+        IP=$(
+            aws ec2 describe-instances\
+            --instance-ids $INSTANCE_ID\
+            --query 'Reservations[].Instances[].PublicIpAddress' \
+            --output text
+          )
+    fi
+done
+
+
+ 
+    
