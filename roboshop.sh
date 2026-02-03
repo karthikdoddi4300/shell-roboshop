efd483319f5cae4429be4a6d2fc092793872ea88
karthikdoddi4300 <karthikdoddi4300@gmail.com>
2026-02-03 20:14:17 +0530

    shell-practice

--- roboshop.sh
+++ roboshop.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 AMI_ID="ami-0220d79f3f480ecf5"
 SGID="sg-0d9028f369846db6c"
+HOSTED_ZONE="Z05627073KQ1HLECLE2G7 "
+DOMAIN_NAME="hodophile.online"
 
 for instance in $@
 do 
@@ -20,6 +22,7 @@ do
             --query 'Reservations[].Instances[].PublicIpAddress' \
             --output text
           )
+          RECORD_NAME="$DOMAIN_NAME" # hodophile.online
     else 
         IP=$(
             aws ec2 describe-instances\
@@ -27,8 +30,34 @@ do
             --query 'Reservations[].Instances[].PrivateIpAddress' \
             --output text
           )
+          RECORD_NAME="$instance.$DOMAIN_NAME" #mongodb.hodophile.online
     fi
     echo "ip address : $IP"
+
+aws route53 change-resource-record-sets \
+    --hosted-zone-id $HOSTED_ZONE \
+    --change-batch '
+        {
+        "Comment": "Update A record to new IP address",
+        "Changes": [
+            {
+            "Action": "UPSERT",
+            "ResourceRecordSet": {
+                "Name": "'$RECORD_NAME'",
+                "Type": "A",
+                "TTL": 300,
+                "ResourceRecords": [
+                {
+                    "Value": "'$IP'"
+                }
+                ]
+            }
+            }
+        ]
+        }
+
+        '
+
 done
 
 
